84f5648bde501b86a23870d8abc68795dad2cccd
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-01-19 21:38:53 +0300

    ## updated test

--- compiler/testData/diagnostics/testsWithJsStdLib/jsCode/error.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/jsCode/error.kt
@@ -1,7 +1,14 @@
+val code = """
+    var s = "hello"
+    + );
+"""
+
 fun main(args: Array<String>): Unit {
     js("var<!JSCODE_ERROR!> =<!> 10;")
 
     js("""var<!JSCODE_ERROR!> =<!> 10;""")
 
     js(<!JSCODE_ERROR!>"var " + " = " + "10;"<!>)
+
+    js(<!JSCODE_ERROR!>code<!>)
 }
