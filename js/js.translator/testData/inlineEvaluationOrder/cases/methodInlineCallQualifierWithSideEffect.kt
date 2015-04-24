5a5762ed8fa68ba71fa15af1da18a061a340ae46
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-04-24 22:11:38 +0300

    test

--- js/js.translator/testData/inlineEvaluationOrder/cases/methodInlineCallQualifierWithSideEffect.kt
+++ js/js.translator/testData/inlineEvaluationOrder/cases/methodInlineCallQualifierWithSideEffect.kt
@@ -4,11 +4,11 @@ package foo
 // CHECK_NOT_CALLED: plus
 
 class A(val value: Int) {
-    inline fun plus(num: Int): Int = this.value + num
+    inline fun add(num: Int): Int = this.value + num
 }
 
 fun box(): String {
-    assertEquals(15, A(fizz(5)) + buzz(10))
+    assertEquals(15, A(fizz(5)).add(buzz(10)))
     assertEquals("fizz(5);buzz(10);", pullLog())
 
     return "OK"
