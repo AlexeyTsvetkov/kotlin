9508d5b21f620c286d3d57e768539c936367c490
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-10-16 21:02:12 +0300

    Drop @inline from tests and incremental compilation

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/inlineCodegenUtils.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/inlineCodegenUtils.kt
@@ -18,19 +18,18 @@ package org.jetbrains.kotlin.codegen.inline
 
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptorWithSource
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.load.kotlin.FileBasedKotlinClass
 import org.jetbrains.kotlin.load.kotlin.KotlinJvmBinarySourceElement
 import org.jetbrains.kotlin.load.kotlin.VirtualFileKotlinClass
+import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader
 import org.jetbrains.kotlin.load.kotlin.incremental.components.IncrementalCache
 import org.jetbrains.kotlin.resolve.jvm.JvmClassName
-import org.jetbrains.kotlin.resolve.source.PsiSourceElement
+import org.jetbrains.kotlin.serialization.Flags
+import org.jetbrains.kotlin.serialization.ProtoBuf
+import org.jetbrains.kotlin.serialization.deserialization.NameResolver
+import org.jetbrains.kotlin.serialization.deserialization.TypeTable
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedSimpleFunctionDescriptor
-
-public val FunctionDescriptor.sourceFilePath: String
-    get() {
-        val source = source as PsiSourceElement
-        val containingFile = source.psi?.containingFile
-        return containingFile?.virtualFile?.canonicalPath!!
-    }
+import org.jetbrains.kotlin.serialization.jvm.JvmProtoBufUtil
 
 public fun FunctionDescriptor.getClassFilePath(cache: IncrementalCache): String {
     val container = containingDeclaration as? DeclarationDescriptorWithSource
@@ -48,4 +47,42 @@ public fun FunctionDescriptor.getClassFilePath(cache: IncrementalCache): String 
             cache.getClassFilePath(className)
         }
     }
+}
+
+public fun inlineFunctionsJvmNames(bytes: ByteArray): Set<String> {
+    val header = readKotlinHeader(bytes)
+
+    return when (header.kind) {
+        KotlinClassHeader.Kind.CLASS -> {
+            val classData = JvmProtoBufUtil.readClassDataFrom(bytes, header.strings!!)
+            inlineFunctionsJvmNames(classData.classProto.functionList, classData.nameResolver, classData.classProto.typeTable)
+        }
+        KotlinClassHeader.Kind.PACKAGE_FACADE -> {
+            val packageData = JvmProtoBufUtil.readPackageDataFrom(bytes, header.strings!!)
+            inlineFunctionsJvmNames(packageData.packageProto.functionList, packageData.nameResolver, packageData.packageProto.typeTable)
+        }
+        else -> emptySet<String>()
+    }
+}
+
+private fun inlineFunctionsJvmNames(functions: List<ProtoBuf.Function>, nameResolver: NameResolver, protoTypeTable: ProtoBuf.TypeTable): Set<String> {
+    val typeTable = TypeTable(protoTypeTable)
+    val inlineFunctions = functions.filter { Flags.IS_INLINE.get(it.flags) }
+    val jvmNames = inlineFunctions.map {
+        JvmProtoBufUtil.getJvmMethodSignature(it, nameResolver, typeTable)
+    }
+    return jvmNames.filterNotNull().toSet()
+}
+
+private fun readKotlinHeader(bytes: ByteArray): KotlinClassHeader {
+    var header: KotlinClassHeader? = null
+
+    FileBasedKotlinClass.create(bytes) { className, classHeader, innerClasses ->
+        header = classHeader
+        null
+    }
+
+    if (header == null) throw AssertionError("Could not read kotlin header from byte array")
+
+    return header!!
 }
