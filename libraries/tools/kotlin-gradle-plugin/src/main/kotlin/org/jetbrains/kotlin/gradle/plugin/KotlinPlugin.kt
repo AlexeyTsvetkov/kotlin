72db7e44ee1b6591db734dd21f4474a01c0d1080
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-03-15 05:43:06 +0300

    ~ fix java is always recompiled (todo: android?)

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPlugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPlugin.kt
@@ -156,6 +156,9 @@ class Kotlin2JvmSourceSetProcessor(
                 logger.kotlinDebug("Adding kotlin output directory $kotlinDestinationDir to task ${javaTask.name}")
                 javaTask.classpath += project.files(kotlinDestinationDir)
             }
+            javaTask.doLast {
+                javaTask.classpath -= project.files(kotlinDestinationDir)
+            }
         }
 
         val kotlinAnnotationProcessingDep = cachedKotlinAnnotationProcessingDep ?: run {
