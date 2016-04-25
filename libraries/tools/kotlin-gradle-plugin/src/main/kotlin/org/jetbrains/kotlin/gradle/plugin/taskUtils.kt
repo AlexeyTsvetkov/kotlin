9b797b4cfaf7c0bab4fea262de4aee0165864f2d
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-04-25 17:44:36 +0300

    Prevent compile tasks from running when nothing changes
        #KT-10932 fixed

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/taskUtils.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/taskUtils.kt
@@ -1,9 +1,44 @@
 package org.jetbrains.kotlin.gradle.plugin
 
+import org.gradle.api.file.FileCollection
 import org.gradle.api.internal.AbstractTask
 import org.gradle.api.tasks.compile.AbstractCompile
 import java.io.File
 import kotlin.reflect.KProperty
+
+/*
+ * Adding fake up-to-date check to do something before execution, and before input snapshots are taken.
+ * Note, this hack is sensetive to up-to-date checks order, so it should be added before any custom up-to-date checks
+ * that could return false.
+ */
+internal fun AbstractCompile.updateClasspathBeforeTask(newClassPath: ()->FileCollection) {
+    // Gradle can take inputs snapshots before task run (normally) or after task run (--rerun-tasks or some up-to-date when returned false).
+    // Fake up-to-date check to update classpath dynamically before inputs snapshot is taken.
+    // Won't be called in case of some other up-to-date check returned false before this one evaluation
+    // Won't be called in case of --rerun-tasks
+    var classpathIsUpdated = false
+    outputs.upToDateWhen {
+        classpath = newClassPath()
+        classpathIsUpdated = true
+        true
+    }
+
+    // in case fake up-to-date check was not called (see comment above)
+    doFirst {
+        if (!classpathIsUpdated) {
+            classpath = newClassPath()
+        }
+    }
+}
+
+internal fun AbstractCompile.appendClasspathDynamically(file: File) {
+    doFirst {
+        classpath += project.files(file)
+    }
+    doLast {
+        classpath -= project.files(file)
+    }
+}
 
 internal var AbstractTask.anyClassesCompiled by TaskPropertyDelegate<Boolean?>("anyClassesCompiled")
 internal var AbstractTask.kotlinDestinationDir by TaskPropertyDelegate<File?>("kotlinDestinationDir")
