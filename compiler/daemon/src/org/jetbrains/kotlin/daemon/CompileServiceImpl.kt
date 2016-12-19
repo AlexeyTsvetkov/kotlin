aab5c2c4199760bd5dc9c4a0217d2f0741e6e896
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-12-19 08:26:00 +0300

    ~ jar cache

--- compiler/daemon/src/org/jetbrains/kotlin/daemon/CompileServiceImpl.kt
+++ compiler/daemon/src/org/jetbrains/kotlin/daemon/CompileServiceImpl.kt
@@ -678,7 +678,7 @@ class CompileServiceImpl(
         }
     }
 
-    private fun clearJarCache() {
+    override fun clearJarCache() {
         ZipHandler.clearFileAccessorCache()
         (KotlinCoreEnvironment.applicationEnvironment?.jarFileSystem as? CoreJarFileSystem)?.clearHandlersCache()
     }
