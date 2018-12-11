961186a6f14d39708396810faf5883d1d311244f
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2018-12-11 15:07:42 +0300

    ~ dump

--- plugins/jvm-abi-gen/src/org/jetbrains/kotlin/jvm/abi/asm/InnerClassesCollectingVisitor.kt
+++ plugins/jvm-abi-gen/src/org/jetbrains/kotlin/jvm/abi/asm/InnerClassesCollectingVisitor.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.kotlin.jvm.abi.asm
 
 import org.jetbrains.org.objectweb.asm.ClassVisitor
+import org.jetbrains.org.objectweb.asm.MethodVisitor
+import org.jetbrains.org.objectweb.asm.Opcodes
 
 internal class InnerClassesCollectingVisitor : ClassVisitor(AbiExtensionAsmApiVersion) {
     lateinit var ownInternalName: String
@@ -25,5 +27,23 @@ internal class InnerClassesCollectingVisitor : ClassVisitor(AbiExtensionAsmApiVe
     override fun visitInnerClass(name: String, outerName: String?, innerName: String?, access: Int) {
         super.visitInnerClass(name, outerName, innerName, access)
         myInnerClasses.add(name)
+    }
+
+    override fun visitMethod(access: Int, name: String?, desc: String?, signature: String?, exceptions: Array<out String>?): MethodVisitor {
+        val mv = super.visitMethod(access, name, desc, signature, exceptions)
+        return object : MethodVisitor(AbiExtensionAsmApiVersion, mv) {
+            override fun visitTypeInsn(opcode: Int, type: String?) {
+                super.visitTypeInsn(opcode, type)
+            }
+
+            override fun visitLdcInsn(cst: Any?) {
+                Opcodes.GETSTATIC
+                super.visitLdcInsn(cst)
+            }
+
+            override fun l(opcode: Int, owner: String?, name: String?, desc: String?) {
+                super.visitFieldInsn(opcode, owner, name, desc)
+            }
+        }
     }
 }
