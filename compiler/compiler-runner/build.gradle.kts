d3cb212d899d4d728056efb1822792422b56af7e
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-04-25 19:33:53 +0300

    ~ remove deps from kotlin compiler runner

--- compiler/compiler-runner/build.gradle.kts
+++ compiler/compiler-runner/build.gradle.kts
@@ -11,13 +11,9 @@ plugins {
 dependencies {
     compile(project(":kotlin-build-common"))
     compileOnly(project(":compiler:cli-common"))
-    compileOnly(project(":kotlin-preloader"))
-    compileOnly(project(":compiler:frontend.java"))
     compileOnly(project(":compiler:daemon-common"))
     compile(projectRuntimeJar(":kotlin-daemon-client"))
     compileOnly(project(":compiler:util"))
-    compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
-    runtimeOnly(projectRuntimeJar(":kotlin-compiler-embeddable"))
 }
 
 sourceSets {
