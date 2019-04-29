45fec00d80bb8d417d21afae30ed0d33e83ed0bf
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-04-30 02:17:07 +0300

    ~ remove weird deps

--- libraries/tools/kotlin-gradle-plugin/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin/build.gradle.kts
@@ -51,24 +51,11 @@ dependencies {
     compile(kotlinStdlib())
     compile(project(":kotlin-native:kotlin-native-utils"))
 
-    compile("com.google.code.gson:gson:${rootProject.extra["versions.jar.gson"]}")
-
     modulesToInclude.forEach {
         jarContents(compileOnly(project(it)) { isTransitive = false })
     }
-
-    compileOnly("com.android.tools.build:gradle:2.0.0")
-    compileOnly("com.android.tools.build:gradle-core:2.0.0")
-    compileOnly("com.android.tools.build:builder:2.0.0")
-    compileOnly("com.android.tools.build:builder-model:2.0.0")
-    compileOnly("org.codehaus.groovy:groovy-all:2.4.12")
+    
     compileOnly(gradleApi())
-
-    runtime(projectRuntimeJar(":kotlin-compiler-embeddable"))
-    runtime(projectRuntimeJar(":kotlin-annotation-processing-gradle"))
-    runtime(projectRuntimeJar(":kotlin-android-extensions"))
-    runtime(projectRuntimeJar(":kotlin-scripting-compiler-embeddable"))
-    runtime(projectRuntimeJar(":kotlin-scripting-compiler-impl-embeddable"))
     runtime(project(":kotlin-reflect"))
 
     jarContents(compileOnly(intellijDep()) {
